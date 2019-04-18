fa800d799f334444a937d9149a64e7764c4d4cf4
HyoKong <32049327+HyoKong@users.noreply.github.com>
2019-04-18 17:00:21 +0800

    add file loader(not completed)

--- WpfApp1/RealTimeDisplay.xaml.cs
+++ WpfApp1/RealTimeDisplay.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Threading;
 using Microsoft.Research.DynamicDataDisplay;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
 using System.Collections;
+using System.IO;
+using TEST;
 
 namespace WpfApp1
 {
@@ -53,8 +55,6 @@ namespace WpfApp1
 
             Point point = new Point(x, y);
             dataSource.AppendAsync(base.Dispatcher, point);
-            
-
 
             if (true)
             {
@@ -86,7 +86,11 @@ namespace WpfApp1
                 //Console.WriteLine(xaxis.ToString());
 
                 //Debug.Write("{0}\n", yaxis.ToString());
-                plotter.Viewport.Visible = new System.Windows.Rect(xaxis, 10, group, yaxis);//主要注意这里一行  
+                dllPlotter.Viewport.Visible = new System.Windows.Rect(xaxis, 0, group, yaxis);//主要注意这里一行  
+                dyPlotter.Viewport.Visible = new System.Windows.Rect(xaxis, 0, group, yaxis);
+                fsjPlotter.Viewport.Visible = new System.Windows.Rect(xaxis, 0, group, yaxis);
+                tcyPlotter.Viewport.Visible = new System.Windows.Rect(xaxis, 0, group, yaxis);
+
             }
 
             //dataSource = new ObservableDataSource<Point>();
@@ -96,13 +100,21 @@ namespace WpfApp1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            plotter.AddLineGraph(dataSource, Colors.Green, 2, "Percentage");
+            dllPlotter.AddLineGraph(dataSource, Colors.Green, 2, "Percentage");
+            dyPlotter.AddLineGraph(dataSource, Colors.Green, 2, "percentage");
+            fsjPlotter.AddLineGraph(dataSource, Colors.Green, 2, "percentage");
+            tcyPlotter.AddLineGraph(dataSource, Colors.Green, 2, "percentage");
             //plotter.AddLineChart(dataSource);
-            plotter.LegendVisible = true;
+            dllPlotter.LegendVisible = true;
+            dyPlotter.LegendVisible = true;
+            fsjPlotter.LegendVisible = true;
+            tcyPlotter.LegendVisible = true;
             dispatcherTimer.Interval = TimeSpan.FromSeconds(0.01);
             dispatcherTimer.Tick += AnimatedPlot;
             dispatcherTimer.IsEnabled = true;
-            plotter.Viewport.FitToView();
+            dllPlotter.Viewport.FitToView();
+            dyPlotter.Viewport.FitToView();
+
         }
 
         private void IfDynamic_Click(object sender, RoutedEventArgs e)
@@ -116,5 +128,130 @@ namespace WpfApp1
                 buttonbool = true;
             }
         }
+    }
+
+    public partial class ReadFile
+    {
+        // txt file name list
+        public string[] dllFile = getFileName("./Dataset/DLL");
+        public string[] dyFile = getFileName("./Dataset/DY");
+        public string[] fsjFile = getFileName("./Dataset/FSJ");
+        public string[] tcyFile = getFileName("./Dataset/TCY");
+
+        public int read_first_num = 100;
+
+        public ReadFile(string part)
+        {
+            if (part == "DLL")
+            {
+                ReadDLL(out string time, out double pro);
+            }
+            else if (part == "DY")
+            {
+                ReadDY();
+            }
+            else if (part=="FSJ")
+            {
+                ReadFSJ();
+            }
+            else
+            {
+                ReadTCY();
+            }
+        }
+ 
+        public void ReadDLL(out string time,out double pro)
+        {
+            // for each txt file of DLL
+            // Unreachable code detected
+            for (int t = 0; t < dllFile.Length; t++)
+            // Unreachable code detected
+            {
+                using (StreamReader dllStreamReader = new StreamReader("./Dataset/DLL/" + dllFile[t]))
+                {
+                   
+
+                    string dllLine = dllStreamReader.ReadLine();
+                    string[] line_sp = dllLine.Split('\t');
+                    double[] value_nums = new double[10];
+                    value_nums[0] = FindFaultData(2.0, -2.0, double.Parse(line_sp[1]));
+                    value_nums[1] = FindFaultData(2.0, -2.0, double.Parse(line_sp[2]));
+                    value_nums[2] = FindFaultData(2.0, -2.0, double.Parse(line_sp[4]));
+                    value_nums[3] = FindFaultData(2.0, -2.0, double.Parse(line_sp[5]));
+                    value_nums[4] = FindFaultData(45.0, -5.0, double.Parse(line_sp[7]));
+                    value_nums[5] = FindFaultData(45.0, -5.0, double.Parse(line_sp[8]));
+                    value_nums[6] = FindFaultData(45.0, -5.0, double.Parse(line_sp[9]));
+                    value_nums[7] = FindFaultData(45.0, -5.0, double.Parse(line_sp[10]));
+                    value_nums[8] = FindFaultData(45.0, -5.0, double.Parse(line_sp[11]));
+                    value_nums[9] = FindFaultData(45.0, -5.0, double.Parse(line_sp[12]));
+
+                    pro = TEST.Class1.test_dll(value_nums);
+
+                    time = line_sp[0];
+                    
+                }
+            } 
+        }
+
+        public void ReadDY()
+        {
+
+        }
+        public void ReadFSJ()
+        {
+
+        }
+        
+        public void ReadTCY()
+        {
+
+        }
+
+        // Eliminate fault data caused by transcoding or other reasons
+        private double FindFaultData(double high, double low, double data)
+        {
+            if (data >= high * 1000)
+            {
+                return (low + high) / 2;
+            }
+            return data;
+
+        }
+        #region 读取文件夹下所有txt文件(按文件创建时间排序)
+        //按照文件创建时间排序
+        public static void SortAsFileCreationTime(ref FileInfo[] arrFi)
+        {
+            Array.Sort(arrFi, delegate (FileInfo x, FileInfo y)
+            {
+                return x.LastWriteTime.CompareTo(y.LastWriteTime);
+            }
+            );
+        }
+
+        //读取指定路径下所有文件名d
+        public static string[] getFileName(string path)
+        {
+            DirectoryInfo root = new DirectoryInfo(path);
+            FileInfo[] fsinfos = root.GetFiles("*.*");
+            //string[] txt_name_list = new string[]{};
+            List<string> strList = new List<string>();
+            SortAsFileCreationTime(ref fsinfos);
+            foreach (FileSystemInfo fsinfo in fsinfos)
+            {
+
+                if (fsinfo is DirectoryInfo)
+                {
+                    getFileName(fsinfo.Name);
+                }
+                else
+                {
+                    //Console.WriteLine(fsinfo.Name);
+                    strList.Add(fsinfo.Name);
+                }
+            }
+            string[] strArray = strList.ToArray();
+            return strArray;
+        }
+        #endregion
     }
 }
